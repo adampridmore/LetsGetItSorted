5d083ea627939eab970b34ace916bce88c665b2a
Adam Pridmore <adam_pridmore@hotmail.com>
2018-01-29 22:20:09 +0000

    - Added a quick sort.  - Simplified Bubble Sort pattern mathc.

--- script.fsx
+++ script.fsx
@@ -9,17 +9,31 @@ let sortList1 = List.sort
 let rec sortList2 list = 
     match list with
     | [] -> []
-    | [a] -> [a]
-    | list -> 
-        let firstValue = list |> Seq.head
-        let left = list |> List.filter (fun x -> x < firstValue)
-        let right =  list |> List.filter (fun x -> x > firstValue)
-        List.concat[ (left |> sortList2) ; [firstValue] ; (right |> sortList2)]
+    | pivot::rest -> 
+        let left = rest |> List.filter (fun x -> x < pivot)
+        let right =  rest |> List.filter (fun x -> x > pivot)
+        List.concat[ (left |> sortList2) ; [pivot] ; (right |> sortList2)]
+
+//randomList 100 |> sortList2
+
+// Bubble sort
+let sortList3 list = 
+    let mutable array = list |> List.toArray
+    let mutable fin = false
+
+    while not fin do
+        fin <- true
+        for i = 0 to array.Length - 2 do
+            if array.[i] > array.[i+1] then
+                let mutable t = array.[i]
+                array.[i] <- array.[i+1]
+                array.[i+1] <- t
+                fin <- false
+
+    array |> Array.toList
 
 [1;7;3;5;6;2;9;4;8]
-|> sortList2
+|> sortList3
 |> (printfn "%A")
 
-randomList 100 |> sortList2
 
-            
